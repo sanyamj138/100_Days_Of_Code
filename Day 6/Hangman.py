508690f614b3ca381b9448f9d15faca87b5bb6d2
Sanyam Jain <sanyamj138@gmail.com>
2026-02-10 23:42:23 +0530

    Day 7 Commit 1
    Comment Added

---
+++ Day 6/Hangman.py
@@ -0,0 +1,10 @@
+# First Step -> Create a Word List
+# Second Step -> Select a Random Word
+# Third Step -> Now ask user for a letter
+# Fourth Step -> If the letter in the word enter its position
+
+import random
+
+wordList = 
+
+def hangman(word):
