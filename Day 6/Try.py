890bcb0723796999e1ae57697f0c35b619c060d1
Sanyam Jain <sanyamj138@gmail.com>
2026-02-11 00:20:33 +0530

    Day 7 Commit 5
    Basic Idea Developed

---
+++ Day 6/Try.py
@@ -0,0 +1,26 @@
+import random
+
+wordList = ['camel', 'goat', 'horse', 'cat', 'dog']
+randomWord = random.choice(wordList)
+lives = 5
+listCollect = []
+for i in range(0,len(randomWord)):
+    listCollect.append('_')
+
+while lives != 0:
+
+    letterAsk = input("Guess a letter: ")
+    check = False
+    for i in range(0,len(randomWord)):
+        if (letterAsk == randomWord[i]):
+            listCollect[i] = letterAsk
+            check = True
+
+    if check == False:
+        lives -= 1
+        print("You guessed " + letterAsk + " and it's not in the word. You Lost a Life!")
+        print(str(lives) + " Out of 5 Lives Left")
+    else:
+        print(listCollect)
+
+print("Game Over")
