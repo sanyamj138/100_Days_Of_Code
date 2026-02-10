ce19cbd428d122bb8f8fe64c09cf422b9b36d163
Sanyam Jain <sanyamj138@gmail.com>
2026-02-11 00:30:31 +0530

    Day 7 Commit 6
    Final Game Uploaded

--- Day 6/Hangman.py
+++ Day 6/Hangman.py
@@ -7,30 +7,39 @@ import random
 
 wordList = ['camel', 'goat', 'horse', 'cat', 'dog']
 
-
 def hangman():
     randomWord = random.choice(wordList)
-    listCollect = ''
     lives = 5
+    listCollect = []
+    for i in range(0, len(randomWord)):
+        listCollect.append('_')
 
     while lives != 0:
+        finalList = ''
         letterAsk = input("Guess a letter: ")
         check = False
-
-        for j in range(0, len(randomWord)):
-            if (letterAsk == randomWord[j]):
-                listCollect = listCollect + letterAsk
+        for i in range(0, len(randomWord)):
+            if (letterAsk == randomWord[i]):
+                listCollect[i] = letterAsk
                 check = True
-            else:
-                listCollect = listCollect + '_'
-
-        if check == True:
-            print(listCollect)
 
         if check == False:
-            print("You guessed " + letterAsk + " and it's not in the word. You Lost a Life!" )
-            lives = lives - 1
+            lives -= 1
+            print("You guessed " + letterAsk + " and it's not in the word. You Lost a Life!")
             print(str(lives) + " Out of 5 Lives Left")
+        else:
+            for i in range(0, len(randomWord)):
+                finalList += listCollect[i]
+            print(finalList)
+
+        if(finalList == randomWord):
+            print("You Win!")
+            lives = 0
+
+    print("Game Over!")
 
 
-hangman()
+print("Welcome to Hangman!")
+inp = input("Write Hangman to Play the Game: ")
+if inp == "Hangman":
+    hangman()
--- Day 6/Try.py
+++ Day 6/Try.py
@@ -1,28 +1,3 @@
 import random
 
 wordList = ['camel', 'goat', 'horse', 'cat', 'dog']
-randomWord = random.choice(wordList)
-lives = 5
-listCollect = []
-for i in range(0,len(randomWord)):
-    listCollect.append('_')
-
-while lives != 0:
-    finalList = ''
-    letterAsk = input("Guess a letter: ")
-    check = False
-    for i in range(0,len(randomWord)):
-        if (letterAsk == randomWord[i]):
-            listCollect[i] = letterAsk
-            check = True
-
-    if check == False:
-        lives -= 1
-        print("You guessed " + letterAsk + " and it's not in the word. You Lost a Life!")
-        print(str(lives) + " Out of 5 Lives Left")
-    else:
-        for i in range(0,len(randomWord)):
-            finalList += listCollect[i]
-        print(finalList)
-
-print("Game Over!")
