11ea65f731b08b59230dcb48567f2b2514762105
Sanyam Jain <sanyamj138@gmail.com>
2026-02-15 00:37:48 +0530

    Day 8 Commit 3
    Added a new dictionary

--- Day 8/Grade Calculator.py
+++ Day 8/Grade Calculator.py
@@ -1,8 +1,14 @@
 print("Welcome to the Grade Calculator!")
+
 numStudents = int(input("Enter the Number of Students: "))
 dict = {}
 
 for i in range(numStudents):
-    name = input('Enter Name of Student' + str(i + 1) +' : ')
-    dict[name] = ''
+    name = input('Enter Name of Student ' + str(i + 1) +' : ')
+    number = int(input('Enter Number of Student ' + str(i+1) + ': '))
+    dict[name] = number
+
+gradeDict = {}
+for i, j in dict.items():
+    gradeDict[i] = ''
 
