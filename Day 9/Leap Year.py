eb3940d4c9ebee5b8fe14b854f8d823eba416978
Sanyam Jain <sanyamj138@gmail.com>
2026-02-15 00:57:24 +0530

    Day 9 Commit 2
    Final Code added

--- Day 9/Leap Year.py
+++ Day 9/Leap Year.py
@@ -1,2 +1,10 @@
 print("Welcome to the Leap Year!")
 
+def is_leap_year(year):
+    if((year%4 == 0 and year%100 != 0) or (year%400 == 0)):
+        return True
+    else:
+        return False
+
+yearIn = int(input("Enter the Year you want to check for: "))
+print(is_leap_year(yearIn))
