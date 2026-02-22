b9bc89cca2f52aa80f8a60be77a8dca17550284d
Sanyam Jain <sanyamj138@gmail.com>
2026-02-22 16:18:41 +0530

    Day 14 Commit 3
    Data Updated

--- Day 14/High Lower Game/dataFile.py
+++ Day 14/High Lower Game/dataFile.py
@@ -1,46 +1,46 @@
 data = [
     {
         "name" : "Virat Kohli",
-        "follower_count" : "270000000",
+        "follower_count" : 270,
     },
     {
         "name": "Cristiano Ronaldo",
-        "follower_count": "670000000",
+        "follower_count": 670,
     },
     {
         "name": "Lionel Messi",
-        "follower_count": "570000000",
+        "follower_count": 570,
     },
     {
         "name": "Selena Gomez",
-        "follower_count": "415000000",
+        "follower_count": 415,
     },
     {
         "name": "Shraddha Kapoor",
-        "follower_count": "94000000",
+        "follower_count": 94,
     },
     {
         "name": "Priyanka Chopra",
-        "follower_count": "92000000",
+        "follower_count": 92,
     },
     {
         "name": "Dwayne Johnson",
-        "follower_count": "390000000",
+        "follower_count": 390,
     },
     {
         "name": "Ariana Grande",
-        "follower_count": "372000000",
+        "follower_count": 372,
     },
     {
         "name": "Alia Bhatt",
-        "follower_count": "86000000",
+        "follower_count": 86,
     },
     {
         "name": "Deepika Padukone",
-        "follower_count": "80000000",
+        "follower_count": 80,
     },
     {
         "name": "Narendra Modi",
-        "follower_count": "92000000",
+        "follower_count": 92,
     },
 ]
