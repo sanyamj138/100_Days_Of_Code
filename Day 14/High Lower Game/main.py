3672ff7078894808aadc37c6f6386230e7061b40
Sanyam Jain <sanyamj138@gmail.com>
2026-02-27 21:22:54 +0530

    Day 14 Commit 6
    Main Game Code Added

--- Day 14/High Lower Game/main.py
+++ Day 14/High Lower Game/main.py
@@ -4,9 +4,34 @@ import random
 account_a = random.choice(data)
 account_b = random.choice(data)
 
+def format_data(account):
+    account_name = account['name']
+    return account_name
+
+
 if account_a == account_b:
     account_b = random.choice(data)
 
-account_name = account_a['name']
-account_number = account_a['follower_count']
+print(f"Compare A: {format_data(account_a)}.")
+print("With")
+print(f"Compare B: {format_data(account_b)}.")
 
+ans = input("Who has more followers? Type 'A' or 'B': ")
+
+a_follower_count = account_a['follower_count']
+b_follower_count = account_b['follower_count']
+
+if ans == 'A':
+    if a_follower_count > b_follower_count:
+        print("You Win!")
+    elif a_follower_count < b_follower_count:
+        print("You Lose!")
+    else:
+        print("Draw!")
+else:
+    if a_follower_count > b_follower_count:
+        print("You Lose!")
+    elif a_follower_count < b_follower_count:
+        print("You Win!")
+    else:
+        print("Draw!")
