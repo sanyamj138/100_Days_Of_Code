79b78ef511f862ea6f24ee6228ac6505a133ccca
Sanyam Jain <sanyamj138@gmail.com>
2026-02-27 21:36:46 +0530

    Day 14 Commit 7
    Full Game Code Deployed

--- Day 14/High Lower Game/main.py
+++ Day 14/High Lower Game/main.py
@@ -1,37 +1,50 @@
-from dataFile import data
-import random
+def game_start(time_run):
+    from dataFile import data
+    import random
 
-account_a = random.choice(data)
-account_b = random.choice(data)
-
-def format_data(account):
-    account_name = account['name']
-    return account_name
-
-
-if account_a == account_b:
+    account_a = random.choice(data)
     account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}.")
-print("With")
-print(f"Compare B: {format_data(account_b)}.")
+    if account_a == account_b:
+        account_b = random.choice(data)
 
-ans = input("Who has more followers? Type 'A' or 'B': ")
+    def format_data(account):
+        account_name = account['name']
+        return account_name
 
-a_follower_count = account_a['follower_count']
-b_follower_count = account_b['follower_count']
+    print(f"COMPARE A: {format_data(account_a)}.")
+    print("WITH")
+    print(f"COMPARE B: {format_data(account_b)}.")
 
-if ans == 'A':
-    if a_follower_count > b_follower_count:
-        print("You Win!")
-    elif a_follower_count < b_follower_count:
-        print("You Lose!")
+    ans = input("WHO HAS MORE FOLLOWERS? TYPE 'A' OR 'B': ")
+
+    a_follower_count = account_a['follower_count']
+    b_follower_count = account_b['follower_count']
+
+    if ans == 'A':
+        if a_follower_count > b_follower_count:
+            print("YOU WIN!")
+            return game_start(time_run + 1)
+        elif a_follower_count < b_follower_count:
+            print("YOU LOSE!")
+            return time_run
+        else:
+            print("DRAW!")
+            return game_start(time_run + 1)
     else:
-        print("Draw!")
-else:
-    if a_follower_count > b_follower_count:
-        print("You Lose!")
-    elif a_follower_count < b_follower_count:
-        print("You Win!")
-    else:
-        print("Draw!")
+        if a_follower_count > b_follower_count:
+            print("YOU LOSE!")
+            return time_run
+        elif a_follower_count < b_follower_count:
+            print("YOU WIN!")
+            return game_start(time_run + 1)
+        else:
+            print("DRAW!")
+            return game_start(time_run + 1)
+
+
+
+start = input("WOULD YOU LIKE TO PLAY THIS GAME? TYPE 'Y FOR YES' or 'N FOR NO': ")
+if start == 'Y' or start == 'y':
+    sol = game_start(0)
+    print("YOUR FINAL SCORE: ", sol)
