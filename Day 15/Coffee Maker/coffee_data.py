dae545401ba3c2b9a0265e65b981e2ea7cfd1ca2
Sanyam Jain <sanyamj138@gmail.com>
2026-02-28 09:12:08 +0530

    Day 15 Commit 3
    Data Set Added

--- Day 15/Coffee Maker/coffee_data.py
+++ Day 15/Coffee Maker/coffee_data.py
@@ -1,12 +1,27 @@
-latte = {
-    'Milk' : '',
-    'Water' : '',
-    'Sugar' : '',
-    'Price' : '',
-}
+Menu = {
+    "Esspresso": {
+        "Ingredients": {
+            'Coffee' : 50,
+            'Water' : 20,
+        },
+        'Price' : 200,
+    },
 
-espresso = {
-    'Milk' : '',
-    'Water' : '',
-    'Price' : '',
+    "Lattee": {
+        "Ingredients": {
+            'Milk' : 150,
+            'Water' : 200,
+            'Coffee' : 25,
+        },
+        "Price": 250,
+    },
+
+    "Cappuccino": {
+        "Ingredients": {
+            'Milk' : 100,
+            'Water' : 250,
+            'Coffee' : 25,
+        },
+        "Price": 300,
+    }
 }
