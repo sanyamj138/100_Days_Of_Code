2493d24272fad051c3e069fffb110c095c7c6a0f
Sanyam Jain <sanyamj138@gmail.com>
2026-03-01 23:29:08 +0530

    Day 16 Commit 1
    Data File Added

---
+++ Day 16/Coffee Maker Class and Objects/coffee_data.py
@@ -0,0 +1,27 @@
+Menu = {
+    "espresso": {
+        "Ingredients": {
+            'Coffee' : 50,
+            'Water' : 50,
+        },
+        'Price' : 150,
+    },
+
+    "latte": {
+        "Ingredients": {
+            'Milk' : 150,
+            'Water' : 200,
+            'Coffee' : 25,
+        },
+        "Price": 250,
+    },
+
+    "cappuccino": {
+        "Ingredients": {
+            'Milk' : 100,
+            'Water' : 250,
+            'Coffee' : 25,
+        },
+        "Price": 300,
+    },
+}
